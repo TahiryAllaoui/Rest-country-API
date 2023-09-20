22deefa5cc7904601f4bab44732913efe662174f
TahiryAllaoui <tallaoui31@gmail.com>
2023-09-20 20:30:15 +0300

    Fetching update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CountryDatas, { CountryType } from '../contexts/CountryDatas';
 import Theme from '../contexts/Theme';
@@ -21,28 +21,50 @@ function App() {
   const setCountries = useContext(CountryDatas).setCountries
 
   useEffect(() => {
-    fetch("http://localhost:8000/countries").then((res) => res.json()).then((data: any) => {
+    fetch("https://restcountries.com/v3.1/all").then((res) => res.json()).then((data: any) => {
       const tmp: CountryType[] = [];
       data.forEach((d: any) => {
         const currencies: string[] = [];
-        d.currencies?.forEach((c: any) => currencies.push(c.name))
+        if (d.currencies)
+          Object.entries(d.currencies)?.forEach((c: any) => currencies.push(c[1].name))
 
         const languages: string[] = [];
-        d.languages?.forEach((c: any) => languages.push(c.name))
+        if (d.languages)
+          Object.entries(d.languages)?.forEach((c: any, i: number) => { if (c[i + 1] != undefined) languages.push(c[i + 1]) });
+
+        let myFlag: string = '';
+        if (d.flags) {
+
+          Object.entries(d.flags)?.forEach((c: any) => {
+            for (let i = 0; i < c.length; i++) {
+              if (c[i].includes("https")) {
+                myFlag = c[i];
+                break;
+              }
+            }
+          });
+        }
+
+        let nativeName: string = '';
+        if (d.name.nativeName)
+          Object.entries(d.name.nativeName)?.forEach((c: any) => nativeName = c[0].official)
+
+        let topLevelDomain: string = '';
+        topLevelDomain = d.tld ? d.tld[0] : '';
 
         let c: CountryType = {
-          name: d.name,
-          flag: d.flag,
+          name: d.name.common,
+          flag: myFlag,
           borderCountries: d.borders,
           capital: d.capital,
           currencies: currencies,
           languages: languages,
-          nativeName: d.nativeName,
+          nativeName: nativeName,
           population: d.population,
           region: d.region,
           subRegion: d.subregion,
-          topLevelDomain: d.topLevelDomain,
-          code3Name: d.alpha3Code,
+          topLevelDomain: topLevelDomain,
+          code3Name: d.fifa,
           bordersNames: []
         }
         tmp.push(c);
